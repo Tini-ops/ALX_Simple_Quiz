2c136f80b4ad449d022da12e19d1e6be82f47028
Tentswalo <tintswalototsndlovu@gmail.com>
2025-01-18 17:24:09 +0200

    Commiting all files

---
+++ quiz.js
@@ -0,0 +1,24 @@
+function checkAnswer() {
+    let correctAnswer = 4;
+    console.log('Inside the check answer function');
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
+    console.log(selectedAnswer);
+    
+    if (selectedAnswer) {
+        const userAnswer = selectedAnswer.value;
+        console.log('you chose ' + userAnswer );
+        const feedback = document.getElementById('feedback');
+
+        if (userAnswer == correctAnswer){
+            feedback.textContent = "Correct! Well done.";
+        }else {
+            feedback.textContent = "That's incorrect. Try again!";
+        }
+    }
+    
+}
+
+const submitbutton = document.getElementById('submit-answer');
+console.log(submitbutton);
+submitbutton.addEventListener('click', checkAnswer);
+
